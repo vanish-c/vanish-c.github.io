4fc6e9edaaf952728e77eb5911453fcf2bd59e76
vanish-c <vanish.ll@icloud.com>
2024-12-21 15:43:26 +0000

    1 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/19/hello-world/"];function toRandomPost(){
+var posts=["2024/12/21/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
